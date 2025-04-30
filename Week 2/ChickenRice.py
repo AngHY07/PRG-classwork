90dbfa2c2ecf69fdb76322c0dc6fd3c658df0f8f
AngHY07 <jaydenanghy@gmail.com>
2025-04-30 16:16:12 +0800

    changes

---
+++ Week 2/ChickenRice.py
@@ -0,0 +1,13 @@
+import math
+
+priceofchickenrice = float(input('Kindly enter price of chicken rice '))
+additionalsidedishes = int(input('Kindly enter the number of additional side dishes '))
+
+pricetotaldishes = 1.2*additionalsidedishes
+
+totalcostwithouttax = priceofchickenrice + pricetotaldishes
+totalcostwithtax = totalcostwithouttax + (totalcostwithouttax*0.09)
+
+def roundtonearest1(value):
+    return math.ceil(value*10)/10
+print('Total cost of the purchase is $',roundtonearest1(totalcostwithtax))
---
+++ Week 2/enginerryingcomputation.py
@@ -0,0 +1,24 @@
+import math
+
+radiuscylinder = float(input('Kindly enter the radius of the cylinder in m '))
+heightcylinder = float(input('Kindly enter the height of cylinder in m'))
+
+volumeofcylinder = math.pi*(radiuscylinder**2)*heightcylinder
+
+radiussphere = float (input('Kindly enter the radius of the sphere in m'))
+
+surfaceAreaSphere = 4*math.pi*radiussphere**2
+
+mass1 = float(input('Kindly enter mass of object 1 in kg '))
+mass2= float(input('Kindlt enter mass of object 2 in kg'))
+distance = float(input('Kindly enter the distance between the two object in m '))
+
+g = 6.6743*(10**-11)
+
+forceOfGravity = (g*mass1*mass2) / distance**2
+
+
+print(f'Volume of the cylinder: {volumeofcylinder:.2f} cubic meters')
+print(f'Surface area of the sphere: {surfaceAreaSphere:.2f} square meters')
+print(f'Force of gravity between the objects: {forceOfGravity:.2e} Newtons')
+
