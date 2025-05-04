00c8988bdc1506b2d4e74e8ca9454b20796d65a0
AngHY07 <jaydenanghy@gmail.com>
2025-05-05 00:29:48 +0800

    changes

--- Week 3/friends.py
+++ Week 3/friends.py
@@ -2,7 +2,7 @@ friendlist =["Izzat","Bryan","Dalton","Ethan","Isaac"]
 
 newname = str(input('What is the name of your new friend? '))
 print("My friends are now ",friendlist)
-friendzone = str(input("Who do you want to be friendazoned? "))
+friendzone = str(input("Who do you want to be friendzoned? "))
 position = friendlist.index(friendzone)
 
 friendlist.remove(friendzone)
