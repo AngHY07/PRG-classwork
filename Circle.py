b21a60ea6e8ff49289d976898ff4eec823d43797
AngHY07 <jaydenanghy@gmail.com>
2025-05-05 00:22:48 +0800

    changes

---
+++ Circle.py
@@ -0,0 +1,11 @@
+
+import math 
+
+a = float(input("enter the value of a, "))
+b = float(input("enter the value of b, "))
+
+def area(d,e):
+    c = math.sqrt((d**2 + e**2))
+    return math.pi*((c/2)**2)
+
+print("The area of the circle is ", area(a,b))
