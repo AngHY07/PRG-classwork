de5a93d86a2bc8b439938f046c4e35afb71f6f4b
AngHY07 <jaydenanghy@gmail.com>
2025-05-05 20:17:44 +0800

    c

--- Week 3/friends.py
+++ Week 3/friends.py
@@ -1,12 +1,13 @@
+# Ang Hao Yi (10273989D)
 friendlist =["Izzat","Bryan","Dalton","Ethan","Isaac"]
 
 newname = str(input('What is the name of your new friend? '))
+friendlist.append(newname)
 print("My friends are now ",friendlist)
+
 friendzone = str(input("Who do you want to be friendzoned? "))
 position = friendlist.index(friendzone)
-
 friendlist.remove(friendzone)
-friendlist.append(newname)
 
 print('{} was in position {}. He will be friendzoned'.format(friendzone,position))
 print("My eligible friends are now: ",friendlist)
