fbec3d1cb865145bc66f9be2aff0362944b09365
AngHY07 <jaydenanghy@gmail.com>
2025-05-11 21:44:50 +0800

    c

--- Week 3/Circle.py
+++ Week 3/Circle.py
@@ -1,11 +1,11 @@
 #Ang Hao Yi (10273989D)
 import math 
 
-a = float(input("enter the value of a, "))
-b = float(input("enter the value of b, "))
+a = float(input("Enter the length of a: "))
+b = float(input("Enter the length of b: "))
 
 def area(d,e):
     c = math.sqrt((d**2 + e**2))
     return math.pi*((c/2)**2)
 
-print("The area of the circle is ", area(a,b))
+print("The area of the circle is", area(a,b))
