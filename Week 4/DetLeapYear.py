286186275f916cbae681825f97aa13434367257e
AngHY07 <jaydenanghy@gmail.com>
2025-05-13 12:21:23 +0800

    c

--- Week 4/DetLeapYear.py
+++ Week 4/DetLeapYear.py
@@ -3,7 +3,7 @@
 year = int(input("Please enter the year: "))
 
 
-if year%4 == 0 and (year%100 != 0 or year%400==0):
+if (year%4 == 0 and year%100 != 0) or year%400==0:
     print(f'{year} is a leap year')
 else :
     print(f'{year} is not a leap year')
