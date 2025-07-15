ca4a4f62db70e48e5d7c267c0aa3148f38901f7d
AngHY07 <jaydenanghy@gmail.com>
2025-07-15 12:30:03 +0800

    s

--- Week 13/test.py
+++ Week 13/test.py
@@ -1,3 +1,13 @@
-word = "john"
 
-print(word.split(","))
+
+try: 
+    value = int(input("Enter a value: "))
+    assert value < 25
+    answer = 10/value
+    print("Answer is:",answer)
+except ZeroDivisionError: 
+    print("Cannot be divided by 0")
+except TypeError: 
+    print("Your input type is wrong")
+except AssertionError: 
+    print("Value must be less than 25.")
